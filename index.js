8cd6bd1d1ec17c7ff0c43e8421a344554752f81b
Jen Jones Arnesen <jennifer@dhis2.org>
2020-06-05 18:21:12 +0000

    a22a47f v7.0.4

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _SharingDialog = require('./SharingDialog.component');
+
+var _SharingDialog2 = _interopRequireDefault(_SharingDialog);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _SharingDialog2.default;
